89154bbfe72205558b8ace69c06d40a77f3246ae
Katy Kam <katyky14@gmail.com>
2022-11-01 15:55:20 -0700

    reducer on search, no tested yet

--- react-app/src/store/search.js
+++ react-app/src/store/search.js
@@ -7,6 +7,30 @@ const getSearchAllBusiness = (payload) => {
     }
 }
 
-export const getSearchAllBusinessThunk = (search) => async (dispatch) => {
-    const response = await fetch('/api')
+export const getSearchAllBusinessThunk = (searchData) => async (dispatch) => {
+    console.log('search data in thunk', searchData)
+    const response = await fetch('/api/search/', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({searchData})
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(getSearchAllBusiness(data.business))
+        return data
+    }
+}
+
+
+
+const searchReducer = (state = {}, action) => {
+    switch (action.type) {
+        case SEARCH_ALL_BUSINESS:
+            let searchedBz = action.payload
+            return searchedBz;
+
+        default:
+            return state
+    }
 }
